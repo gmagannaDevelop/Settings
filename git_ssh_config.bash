728f6c58bb7b2c1fc620c9bfd18dac6628a0beb6
gmagannaDevelop <gmaganna.biomed@gmail.com>
2018-12-30 18:13:59 +0000

    Added a couple of pretty-format aliases

--- git_ssh_config.bash
+++ git_ssh_config.bash
@@ -3,6 +3,8 @@
 # Les commandes:
 git config --global user.email "gmaganna.biomed@gmail.com"
 git config --global user.name "gmagannaDevelop"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.st "status -sb"
 cd ~/.ssh || mkdir ~/.ssh # go to dir or create it if it doesn't exist
 cd ~/.ssh && ssh-keygen -t rsa -C "gmganna.biomed@gmail.com"
 
