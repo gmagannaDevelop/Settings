0bb34cd6b1c7dfebc74acdd42e0bad692f3c2a02
gmagannaDevelop <gmaganna.biomed@gmail.com>
2018-12-31 14:58:42 +0000

    Added a line to create an alias 'freecache' that will help keep the cache clean and the live session running in case the available memory is running low because of the cache.

--- live_usb_setup.bash
+++ live_usb_setup.bash
@@ -7,8 +7,11 @@ if [ "$resp" == "Y" ]; then
 	sudo apt-get update;
 	sudo apt-get install vim;
 	sudo apt-get install git;
+        alias freecache='echo 1 | sudo tee > /proc/sys/vm/drop_caches';
 	echo " ";
 	echo "The configuration script finished (check for errors)";
+        echo "The following alias has been defined:";
+        echo "    freecache='echo 1 | sudo tee > /proc/sys/vm/drop_caches'";
 	echo "You can now run the git config script.";
 	echo " ";
 else {
